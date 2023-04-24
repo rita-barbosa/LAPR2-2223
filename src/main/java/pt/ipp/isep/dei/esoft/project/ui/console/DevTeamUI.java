6941785224acd2de3891a3d027b9ae5b84ed7026
rita_barbosa <1220841@isep.ipp.pt>
2023-04-24 20:13:15 +0100

    Update of DevTeamUI class [Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/DevTeamUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/DevTeamUI.java
@@ -12,11 +12,11 @@ public class DevTeamUI implements Runnable {
     public void run() {
         System.out.println("\n");
         System.out.print("Development Team:\n");
-        System.out.print("\t Student Name 1 - 1XXXXXX@isep.ipp.pt \n");
-        System.out.print("\t Student Name 2 - 1XXXXXX@isep.ipp.pt \n");
-        System.out.print("\t Student Name 3 - 1XXXXXX@isep.ipp.pt \n");
-        System.out.print("\t Student Name 4 - 1XXXXXX@isep.ipp.pt \n");
-        System.out.print("\t Student Name 5 - 1XXXXXX@isep.ipp.pt \n");
+        System.out.print("\t Rita Barbosa     1 - 1220841@isep.ipp.pt \n");
+        System.out.print("\t Ana Guterres     2 - 1221933@isep.ipp.pt \n");
+        System.out.print("\t Matilde Varela   3 - 1220683@isep.ipp.pt \n");
+        System.out.print("\t Jonas Souza      4 - 1221597@isep.ipp.pt \n");
+        System.out.print("\t Henrique Ribeiro 5 - 1211487@isep.ipp.pt \n");
         System.out.println("\n");
     }
 }
