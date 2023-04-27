e463b2ef176799e3151d29d1d2660541a2dcdbb2
rita_barbosa <1220841@isep.ipp.pt>
2023-04-28 00:21:03 +0100

    Add JavaDoc for Commission and CommissionType [US002][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Commission.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Commission.java
@@ -1,15 +1,33 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
+import java.util.Objects;
+
 public class Commission {
+    /**
+     * The type of the commission
+     */
     private CommissionType commissionType;
+    /**
+     * The value of the commission.
+     */
     private Double commissionValue;
 
+    /**
+     * Constructs a new Commission instance with the specified commission type and value.
+     * @param commissionType
+     * @param commissionValue
+     */
     public Commission(CommissionType commissionType, Double commissionValue){
         this.commissionValue = commissionValue;
         this.commissionType = commissionType;
     }
 
+    /**
+     * This method creates and returns a copy of a commission instance.
+     * @return  clone of commission instance.
+     */
     public Commission clone(){
         return new Commission(this.commissionType,this.commissionValue);
     }
+
 }
