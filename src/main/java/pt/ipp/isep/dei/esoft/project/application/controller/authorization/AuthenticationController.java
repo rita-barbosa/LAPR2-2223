70ba3721fb097f0649a09c97801a34ec54858d04
rita_barbosa <1220841@isep.ipp.pt>
2023-04-27 13:30:52 +0100

    Update of AuthenticationController - Add Roles [US002][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/authorization/AuthenticationController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/authorization/AuthenticationController.java
@@ -1,19 +1,25 @@
 package pt.ipp.isep.dei.esoft.project.application.controller.authorization;
 
+import pt.ipp.isep.dei.esoft.project.domain.Request;
 import pt.ipp.isep.dei.esoft.project.repository.AuthenticationRepository;
 import pt.ipp.isep.dei.esoft.project.repository.Repositories;
 import pt.isep.lei.esoft.auth.mappers.dto.UserRoleDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Paulo Maio pam@isep.ipp.pt
  */
 public class AuthenticationController {
 
-    public static final String ROLE_ADMIN = "ADMINISTRATOR";
+    public static final String ROLE_ADMIN = "SYSTEM ADMINISTRATOR";
     public static final String ROLE_EMPLOYEE = "EMPLOYEE";
     public static final String ROLE_CLIENT = "CLIENT";
+    public static final String ROLE_STORE_MANAGER = "STORE MANAGER";
+
+    public static final String ROLE_NETWORK_MANAGER = "NETWORK MANAGER";
+
 
     //private final ApplicationSession applicationSession;
     private final AuthenticationRepository authenticationRepository;
