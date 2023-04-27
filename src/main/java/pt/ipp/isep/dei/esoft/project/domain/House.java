a571f470d2282dd8f0cea8c592ee609178c2ddeb
Matilde_Varela <1220683@isep.ipp.pt>
2023-04-27 21:33:41 +0100

    property types related classes updated [US004] [Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/House.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/House.java
@@ -1,19 +1,20 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class House extends Residence {
     private final Boolean basement;
     private final Boolean inhabitableLoft;
     private final Enum<SunExposureTypes> sunExposure;
 
-    public House(PropertyType propertyType, ArrayList<AvailableEquipment> availableEquipment, Double area,
+    public House(PropertyType propertyType, List<String> availableEquipmentDescription, Double area,
                  String streetName, String city, String district, String state, String zipCode,
                  Boolean basement, Boolean inhabitableLoft, Integer parkingSpace, Enum<SunExposureTypes> sunExposure,
                  Integer numberBedroom, Integer numberBathroom, Double distanceCityCenter,
-                 ArrayList<Photograph> photograph) {
-        super(propertyType,area, availableEquipment, streetName, city, district, state, zipCode, parkingSpace, numberBedroom,
-                numberBathroom, distanceCityCenter, photograph);
+                 List<String> uri) {
+        super(propertyType, area, availableEquipmentDescription, streetName, city, district, state, zipCode, parkingSpace,
+                numberBedroom, numberBathroom, distanceCityCenter, uri);
         this.basement = basement;
         this.inhabitableLoft = inhabitableLoft;
         this.sunExposure = sunExposure;
--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Residence.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Residence.java
@@ -1,23 +1,51 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Residence extends Property {
 
     private final Integer numberBedroom;
     private final Integer numberBathroom;
     private final Integer parkingSpace;
-    private final ArrayList<AvailableEquipment> availableEquipment;
+    private final List<AvailableEquipment> availableEquipment;
 
-    public Residence(PropertyType propertyType, Double area, ArrayList<AvailableEquipment> availableEquipment,
+    public Residence(PropertyType propertyType, Double area, List<String> availableEquipmentDescription,
                      String streetName, String city, String district, String state, String zipCode, Integer parkingSpace,
-                     Integer numberBedroom, Integer numberBathroom, Double distanceCityCenter, ArrayList<Photograph> photograph) {
-        
-        super(propertyType, area, distanceCityCenter, photograph, streetName, city, district, state, zipCode);
+                     Integer numberBedroom, Integer numberBathroom, Double distanceCityCenter, List<String> uri) {
+
+        super(propertyType, area, distanceCityCenter, uri, streetName, city, district, state, zipCode);
         this.parkingSpace = parkingSpace;
         this.numberBathroom = numberBathroom;
         this.numberBedroom = numberBedroom;
-        this.availableEquipment = availableEquipment;
+        this.availableEquipment = new ArrayList<>();
+        fillAvailableEquipmentList(availableEquipmentDescription);
+    }
+
+    private void fillAvailableEquipmentList(List<String> availableEquipmentDescription) {
+        for (String description : availableEquipmentDescription) {
+            AvailableEquipment equipment = new AvailableEquipment(description);
+            addAvailableEquipment(equipment);
+        }
+    }
+
+    private void addAvailableEquipment(AvailableEquipment equipment) {
+        if (validate(equipment)) {
+            // A clone of the equipment is added to the list of equipments, to avoid side effects and outside manipulation.
+            this.availableEquipment.add(equipment.clone());
+        }
+    }
+
+    private boolean validate(AvailableEquipment equipment) {
+        return availableEquipmetsDoNotContain(equipment);
+    }
+
+    private boolean availableEquipmetsDoNotContain(AvailableEquipment equipment) {
+        return !this.availableEquipment.contains(equipment);
+    }
+
+    public List<AvailableEquipment> getAvailableEquipment() {
+        return availableEquipment;
     }
 
     public Integer getNumberBedroom() {
@@ -30,9 +58,5 @@ public class Residence extends Property {
 
     public Integer getParkingSpace() {
         return parkingSpace;
-    }
-
-    public ArrayList<AvailableEquipment> getAvailableEquipment() {
-        return availableEquipment;
     }
 }
