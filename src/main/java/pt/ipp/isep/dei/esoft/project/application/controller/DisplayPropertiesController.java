86cc6588668cef0014bfa50a9c7513e9b60d2646
ana_guterres <1221933@isep.ipp.pt>
2023-04-27 01:53:48 +0100

    Add class DisplayPropertiesController [US001][Implementation].

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/DisplayPropertiesController.java
@@ -0,0 +1,10 @@
+package pt.ipp.isep.dei.esoft.project.application.controller;
+
+import pt.ipp.isep.dei.esoft.project.domain.Announcement;
+
+public class DisplayPropertiesController {
+
+    public Announcement announcementList;
+
+
+}
