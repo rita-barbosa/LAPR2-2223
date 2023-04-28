a3a7716ee13d6d8c01867e183a38292599a428b8
Matilde_Varela <1220683@isep.ipp.pt>
2023-04-28 22:54:44 +0100

    Request Test methods [US004] [Implementation]

---
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/RequestTest.java
@@ -0,0 +1,139 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestTest {
+
+    @Test
+    void ensureRequestIsCreatedSuccessfully() {
+        String ownerEmail = "owner@email.com";
+        Employee employee = new Employee("employee@this.app.com", "Agent");
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+    }
+
+    @Test
+    void testEqualsSameObject() {
+        String ownerEmail = "owner@email.com";
+        Employee employee = new Employee("employee@this.app.com", "Agent");
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+
+        assertEquals(request, request);
+    }
+
+    @Test
+    void testEqualsDifferentClass() {
+        String ownerEmail = "owner@email.com";
+        Employee employee = new Employee("employee@this.app.com", "Agent");
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+
+        assertNotEquals(request, new Object());
+    }
+
+    @Test
+    void testEqualsNull() {
+        String ownerEmail = "owner@email.com";
+        Employee employee = new Employee("employee@this.app.com", "Agent");
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+
+        assertNotEquals(request, null);
+    }
+
+    @Test
+    void testEqualsDifferentObject() {
+        String ownerEmail = "owner@email.com";
+        String ownerEmail1 = "owner1@email.com";
+        Employee employee = new Employee("employee@this.app.com", "Agent");
+        Employee employee1 = new Employee("employee1@this.app.com", "Agent");
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        List<String> uriList1 = new ArrayList<>();
+        uriList.add("https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_2x3.jpg");
+        Property property1 = new Property(new PropertyType("Apartment"), 12.6, 150.8, uriList1,
+                "May Av.", "New York", "York", "AL", "58240");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+        Request request1 = new Request(ownerEmail1, property1, new Business("sale", 2043.9), LocalDate.now(), employee1);
+
+        assertNotEquals(request, request1);
+    }
+
+    @Test
+    void testEqualsSameObjectSameDescription() {
+        String ownerEmail = "owner@email.com";
+        Employee employee = new Employee("employee@this.app.com", "Agent");
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+        Request request1 = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+
+        assertEquals(request, request1);
+    }
+
+    @Test
+    void testHashCodeSameObject() {
+        String ownerEmail = "owner@email.com";
+        Employee employee = new Employee("employee@this.app.com", "Agent");
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+
+        assertEquals(request.hashCode(), request.hashCode());
+    }
+
+    @Test
+    void testHashCodeDifferentObject() {
+        String ownerEmail = "owner@email.com";
+        Employee employee = new Employee("employee@this.app.com", "Agent");
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Property property1 = new Property(new PropertyType("House"), (35.5), (89.3), uriList, "street1",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+        Request request1 = new Request(ownerEmail, property1, new Business("sale", 2345.0), LocalDate.now(), employee);
+
+        assertNotEquals(request.hashCode(), request1.hashCode());
+    }
+
+
+    @Test
+    void ensureCloneWorks() {
+        Employee employee = new Employee("john.doe@this.company.org");
+        String ownerEmail = "owner@email.com";
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3), uriList, "street",
+                "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2345.0), LocalDate.now(), employee);
+        Request clone = request.clone();
+        assertEquals(request, clone);
+    }
+}
