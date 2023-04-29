65a1b0d03af74fff84ce6d3e296771b83973fc38
Matilde_Varela <1220683@isep.ipp.pt>
2023-04-29 21:29:17 +0100

    bootstrap update [US004] [Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/Bootstrap.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/Bootstrap.java
@@ -30,9 +30,24 @@ public class Bootstrap implements Runnable {
         Employee employee = new Employee("employee@this.app", "agent");
         Employee employee1 = new Employee("employee1@this.app", "agent");
         Employee employee2 = new Employee("employee2@this.app", "agent");
+
+        Employee employee3 = new Employee("1234", "John Doe", "C12345678",
+                "123-45-6789", "employee3@this.app", "agent", "623 456 7890",
+                "New York", "Manhattan", "NY", "10001", "Broadway");
+
+        Employee employee4 = new Employee("1234", "Annie Smith", "C23456789",
+                "987-65-4321", "employee4@this.app", "agent", "555 555 5555",
+                "New York", "Manhattan", "NY", "10001", "Broadway St");
+
+        Employee employee5 = new Employee("6789", "Maria Silva", "C12345678",
+                "123-45-6789", "employee5@this.app", "agent", "911 234 5567",
+                "Porto", "Porto", "Porto", "43005", "Rua da Alegria, 123");
         agency.addEmployee(employee);
         agency.addEmployee(employee1);
         agency.addEmployee(employee2);
+        agency.addEmployee(employee3);
+        agency.addEmployee(employee4);
+        agency.addEmployee(employee5);
     }
 
     private void addAgencies() {
@@ -46,6 +61,19 @@ public class Bootstrap implements Runnable {
 
         Agency agency3 = new Agency(2627);
         agencyRepository.add(agency3);
+
+        //Integer id, String description, String emailAddress, String phoneNumber, Location location
+
+        Location location = new Location("Saint Avenue", "Heaven", "Sky", "SK", "12345");
+        Agency agency4 = new Agency(1234, "Idealista", "agency4@this.app", "999 444 5656", location);
+        addEmployees(agency4);
+        agencyRepository.add(agency4);
+
+        Location location1 = new Location("Devil's Road", "Hell", "Earth", "ET", "16789");
+        Agency agency5 = new Agency(6789, "RE/MAX", "agency5@this.app", "286 776 8435", location1);
+        addEmployees(agency5);
+        agencyRepository.add(agency5);
+
     }
 
     private void addUsers() {
