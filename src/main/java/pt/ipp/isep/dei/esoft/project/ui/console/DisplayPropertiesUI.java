d900f24dc288c8b09523dc1f098508ae683a834a
ana_guterres <1221933@isep.ipp.pt>
2023-04-30 09:34:02 +0100

    Update DisplayPropertiesUI and add javaDocs [US001][Implementation].

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/DisplayPropertiesUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/DisplayPropertiesUI.java
@@ -7,25 +7,68 @@ import pt.ipp.isep.dei.esoft.project.domain.PropertyType;
 
 import java.util.*;
 
+/**
+ * The DisplayPropertiesUI class implements the Runnable interface and represents the user interface for display the listed properties.
+ */
 public class DisplayPropertiesUI implements Runnable {
 
+    /**
+     * The Controller.
+     */
     private final DisplayPropertiesController controller = new DisplayPropertiesController();
 
+    /**
+     * The type of business of a property.
+     */
     private String businessType;
+    /**
+     * The type of property.
+     */
     private String propertyType;
+    /**
+     * The number of bedrooms in the property.
+     */
     private Integer numberBedrooms;
+    /**
+     * The price of the property.
+     */
     private Double price;
+    /**
+     * The type of sorting for price.
+     */
     private String priceSorting;
+    /**
+     * The name of the city where the property is located.
+     */
     private String city;
+    /**
+     * The type of sorting for city.
+     */
     private String citySorting;
+    /**
+     * The name of the state where the property is located.
+     */
     private String state;
+    /**
+     * The type of sorting for state.
+     */
     private String stateSorting;
 
 
+    /**
+     * This method returns the DisplayPropertiesController instance associated with this UI.
+     *
+     * @return the DisplayAnnouncementsController instance.
+     */
     private DisplayPropertiesController getController() {
         return controller;
     }
 
+    /**
+     * This runs the UI and displays the prompts to the user to obtain the necessary
+     * data to display the listed properties.
+     *
+     */
     public void run() {
         List<Announcement> announcementList = controller.sortAnnouncementsByMostRecentAdded();
 
@@ -84,12 +127,22 @@ public class DisplayPropertiesUI implements Runnable {
         } while (option);
     }
 
+    /**
+     * This method displays announcements.
+     *
+     * @param announcementList the announcement list
+     */
     private void displayAnnouncements(List<Announcement> announcementList) {
         for (Announcement announcement : announcementList) {
             System.out.println(announcement.toString());
         }
     }
 
+    /**
+     * This method asks user if he wants to select any criteria.
+     *
+     * @return {@code true} if user wants to select any criteria;  {@code false} otherwise;
+     */
     private boolean askOption() {
         System.out.println("Do you want to select any criteria? \n1 - Yes \n2 - No");
         Scanner sc = new Scanner(System.in);
@@ -113,6 +166,12 @@ public class DisplayPropertiesUI implements Runnable {
     }
 
 
+    /**
+     * This method display and aks to select criteria from the criteria list.
+     *
+     * @param criterias the list of criterias
+     * @return the criteria chosen
+     */
     private Integer displayAndSelectCriteriaList(List<String> criterias) {
         Scanner sc = new Scanner(System.in);
         int count = 0;
@@ -143,6 +202,11 @@ public class DisplayPropertiesUI implements Runnable {
 
     }
 
+    /**
+     * This method display and aks to select a type of business.
+     *
+     * @return the type of business chosen
+     */
     private String displayAndSelectBusinessType() {
         Scanner sc = new Scanner(System.in);
         List<BusinessType> businessTypes = controller.getBusinessTypes();
@@ -170,6 +234,11 @@ public class DisplayPropertiesUI implements Runnable {
         return null;
     }
 
+    /**
+     * This method display and aks to select a type of property.
+     *
+     * @return the type of property chosen
+     */
     private String displayAndSelectPropertyType() {
         Scanner sc = new Scanner(System.in);
         List<PropertyType> propertyTypes = controller.getPropertyTypes();
@@ -199,6 +268,11 @@ public class DisplayPropertiesUI implements Runnable {
         return null;
     }
 
+    /**
+     * This method display and aks to select number of bedrooms.
+     *
+     * @return the number of bedrooms chosen
+     */
     private Integer displayAndSelectNumberBedrooms() {
         Scanner sc = new Scanner(System.in);
         int option = -1;
@@ -222,21 +296,41 @@ public class DisplayPropertiesUI implements Runnable {
 
     }
 
+    /**
+     * This method display and aks to select a type of sorting to price.
+     *
+     * @return the type of sorting select to price
+     */
     private String displayAndSelectPrice() {
         System.out.println("Price:");
         return sortSelection();
     }
 
+    /**
+     * This method display and aks to select a type of sorting to city.
+     *
+     * @return the type of sorting select to city
+     */
     private String displayAndSelectCity() {
         System.out.println("City:");
         return sortSelection();
     }
 
+    /**
+     * This method display and aks to select a type of sorting to state.
+     *
+     * @return the type of sorting select to state
+     */
     private String displayAndSelectState() {
         System.out.println("State:");
         return sortSelection();
     }
 
+    /**
+     * This method display and aks to select a type of sorting.
+     *
+     * @return the type of sorting chosen
+     */
     private String sortSelection() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Sort types available:");
@@ -265,6 +359,11 @@ public class DisplayPropertiesUI implements Runnable {
         return null;
     }
 
+    /**
+     * This method display the type of business options.
+     *
+     * @param businessTypes the business types
+     */
     private void displayBusinessTypeOptions(List<BusinessType> businessTypes) {
         int count = 0;
         System.out.println("Type of business:");
@@ -274,6 +373,11 @@ public class DisplayPropertiesUI implements Runnable {
         }
     }
 
+    /**
+     * This method display the type of properties options.
+     *
+     * @param propertyTypes the property types
+     */
     private void displayPropertiesTypeOptions(List<PropertyType> propertyTypes) {
         int count = 0;
         System.out.println("Type of properties:");
