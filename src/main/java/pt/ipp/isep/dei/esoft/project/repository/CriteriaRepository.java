bef78311e5f730317c80250619ad0d3b5dbfac1c
Matilde_Varela <1220683@isep.ipp.pt>
2023-04-30 03:09:35 +0100

    CriteriaRepository update [US001] [Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/CriteriaRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/CriteriaRepository.java
@@ -1,9 +1,8 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
-import pt.ipp.isep.dei.esoft.project.domain.Criteria;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 public class CriteriaRepository {
 
