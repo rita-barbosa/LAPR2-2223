8ec470311b5d6b115e08eb850a40d18c936598b4
ana_guterres <1221933@isep.ipp.pt>
2023-04-30 09:34:15 +0100

    Update DisplayPropertiesController and add javaDocs [US001][Implementation].

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/DisplayPropertiesController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/DisplayPropertiesController.java
@@ -12,25 +12,50 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * The type Display properties controller.
+ */
 public class DisplayPropertiesController {
 
+    /**
+     * The Announcement list.
+     */
     private List<Announcement> announcementList;
+    /**
+     * The Agency repository.
+     */
     private AgencyRepository agencyRepository = null;
+    /**
+     * The Criteria repository.
+     */
     private CriteriaRepository criteriaRepository = null;
 
+    /**
+     * The Property type repository.
+     */
     private PropertyTypeRepository propertyTypeRepository = null;
+    /**
+     * The Business type repository.
+     */
     private BusinessTypeRepository businessTypeRepository = null;
 
-    public DisplayPropertiesController(){
+    /**
+     * Instantiates a new Display properties controller.
+     */
+    public DisplayPropertiesController() {
         getAgencyRepository();
         getAgenciesList();
         announcementList = getAnnouncementsList();
-        List<Announcement> clonedAnnouncementList = new ArrayList<>(announcementList);
+        //List<Announcement> clonedAnnouncementList = new ArrayList<>(announcementList);
         sortAnnouncementsByMostRecentAdded();
         getCriteriaRepository();
-
     }
 
+    /**
+     * Gets the agency repository.
+     *
+     * @return the agency repository
+     */
     private AgencyRepository getAgencyRepository() {
         if (agencyRepository == null) {
             Repositories repositories = Repositories.getInstance();
@@ -39,37 +64,57 @@ public class DisplayPropertiesController {
         return agencyRepository;
     }
 
+    /**
+     * Gets the agencies list.
+     *
+     * @return the agencies list
+     */
     public List<Agency> getAgenciesList() {
         AgencyRepository agencyRepository = getAgencyRepository();
         return agencyRepository.getAgenciesList();
     }
 
-    public List<Announcement> getAnnouncementsList(){
+    /**
+     * Get the announcements list from all the agencies.
+     *
+     * @return the announcement list
+     */
+    public List<Announcement> getAnnouncementsList() {
         List<Announcement> announcementList = new ArrayList<>();
         List<Agency> agencies = getAgenciesList();
-            for (Agency agency: agencies) {
+        for (Agency agency : agencies) {
             announcementList.addAll(agency.getAnnouncementsList());
         }
         return announcementList;
     }
 
-    public List<Announcement> sortAnnouncementsByMostRecentAdded(){
+    /**
+     * Sort announcements by most recent added date.
+     *
+     * @return the announcement list sorted
+     */
+    public List<Announcement> sortAnnouncementsByMostRecentAdded() {
         List<Announcement> clonedAnnouncementList = new ArrayList<>(announcementList);
 
         Comparator<Announcement> acceptanceDate = new Comparator<Announcement>() {
-            public int compare(Announcement a1, Announcement a2){
+            public int compare(Announcement a1, Announcement a2) {
                 LocalDate a1AcceptanceDate = a1.getAcceptanceDate();
                 LocalDate a2AcceptanceDate = a2.getAcceptanceDate();
 
                 return a1AcceptanceDate.compareTo(a2AcceptanceDate);
             }
         };
-
-        Collections.sort(clonedAnnouncementList, Collections.reverseOrder(acceptanceDate));
+        clonedAnnouncementList.sort(Collections.reverseOrder(acceptanceDate));
+        //Collections.sort(clonedAnnouncementList, Collections.reverseOrder(acceptanceDate));
         return clonedAnnouncementList;
 
     }
 
+    /**
+     * Gets criteria repository.
+     *
+     * @return the criteria repository
+     */
     public CriteriaRepository getCriteriaRepository() {
         if (criteriaRepository == null) {
             Repositories repositories = Repositories.getInstance();
@@ -79,29 +124,57 @@ public class DisplayPropertiesController {
     }
 
 
-    public List<Announcement> getAnnouncementsByBusinessType(List<Announcement> announcementList, String businessType){
+    /**
+     * Get announcement list by business type.
+     *
+     * @param announcementList the announcement list
+     * @param businessType     the business type
+     * @return the announcement list
+     */
+    public List<Announcement> getAnnouncementsByBusinessType(List<Announcement> announcementList, String businessType) {
         Agency agency = new Agency();
         announcementList = agency.announcementHasBusinessType(announcementList, businessType);
 
         return announcementList;
     }
 
-    public List<Announcement> getAnnouncementsByPropertyType(List<Announcement> announcementList, String propertyType){
+    /**
+     * Get announcements list by property type.
+     *
+     * @param announcementList the announcement list
+     * @param propertyType     the property type
+     * @return the announcement list
+     */
+    public List<Announcement> getAnnouncementsByPropertyType(List<Announcement> announcementList, String propertyType) {
         Agency agency = new Agency();
         announcementList = agency.announcementHasPropertyType(announcementList, propertyType);
 
         return announcementList;
     }
 
-    public List<Announcement> getAnnouncementsByNumberBedrooms(List<Announcement> announcementList, Integer numberBedrooms){
+    /**
+     * Get announcements list by number bedrooms.
+     *
+     * @param announcementList the announcement list
+     * @param numberBedrooms   the number bedrooms
+     * @return the announcement list
+     */
+    public List<Announcement> getAnnouncementsByNumberBedrooms(List<Announcement> announcementList, Integer numberBedrooms) {
         Agency agency = new Agency();
         announcementList = agency.announcementHasNumberBedrooms(announcementList, numberBedrooms);
 
         return announcementList;
     }
 
-    public List<Announcement> getAnnouncementsByPrice(List<Announcement> announcementList, String priceSorting){
-        if (priceSorting.equals("Ascending")){
+    /**
+     * Get announcements list by price.
+     *
+     * @param announcementList the announcement list
+     * @param priceSorting     the price sorting order
+     * @return the announcement list
+     */
+    public List<Announcement> getAnnouncementsByPrice(List<Announcement> announcementList, String priceSorting) {
+        if (priceSorting.equals("Ascending")) {
             Agency agency = new Agency();
             announcementList = agency.sortAnnouncementsByAscendingPrice(announcementList);
             return announcementList;
@@ -112,8 +185,15 @@ public class DisplayPropertiesController {
         }
     }
 
-    public List<Announcement> getAnnouncementsByCity(List<Announcement> announcementList, String citySorting){
-        if (citySorting.equals("Ascending")){
+    /**
+     * Get announcements list by city.
+     *
+     * @param announcementList the announcement list
+     * @param citySorting      the city sorting order
+     * @return the announcement list
+     */
+    public List<Announcement> getAnnouncementsByCity(List<Announcement> announcementList, String citySorting) {
+        if (citySorting.equals("Ascending")) {
             Agency agency = new Agency();
             announcementList = agency.sortAnnouncementsByAscendingCity(announcementList);
             return announcementList;
@@ -121,29 +201,34 @@ public class DisplayPropertiesController {
             Agency agency = new Agency();
             announcementList = agency.sortAnnouncementsByDescendingCity(announcementList);
             return announcementList;
-
         }
     }
 
-    public List<Announcement> getAnnouncementsByState(List<Announcement> announcementList, String stateSorting){
-        if (stateSorting.equals("Ascending")){
+    /**
+     * Get announcements list by state.
+     *
+     * @param announcementList the announcement list
+     * @param stateSorting     the state sorting order
+     * @return the announcement list
+     */
+    public List<Announcement> getAnnouncementsByState(List<Announcement> announcementList, String stateSorting) {
+        if (stateSorting.equals("Ascending")) {
             Agency agency = new Agency();
             announcementList = agency.sortAnnouncementsByAscendingState(announcementList);
             return announcementList;
 
         } else {
             Agency agency = new Agency();
-            announcementList = agency.sortAnnouncementsByDesscendingState(announcementList);
+            announcementList = agency.sortAnnouncementsByDescendingState(announcementList);
             return announcementList;
         }
-
-        }
-
-    public List<BusinessType> getBusinessTypes() {
-        BusinessTypeRepository businessTypeRepository = getBusinessTypeRepository();
-        return businessTypeRepository.getBusinessTypeList();
     }
 
+    /**
+     * Gets the business type repository.
+     *
+     * @return the business type repository
+     */
     private BusinessTypeRepository getBusinessTypeRepository() {
         if (businessTypeRepository == null) {
             Repositories repositories = Repositories.getInstance();
@@ -152,16 +237,36 @@ public class DisplayPropertiesController {
         return businessTypeRepository;
     }
 
-    public List<PropertyType> getPropertyTypes() {
-        PropertyTypeRepository propertyTypeRepository = getPropertyTypeRepository();
-        return propertyTypeRepository.getPropertyTypeList();
+    /**
+     * Gets the list of business types.
+     *
+     * @return the business types
+     */
+    public List<BusinessType> getBusinessTypes() {
+        BusinessTypeRepository businessTypeRepository = getBusinessTypeRepository();
+        return businessTypeRepository.getBusinessTypeList();
     }
 
+    /**
+     * Gets the property type repository.
+     *
+     * @return the property type repository
+     */
     private PropertyTypeRepository getPropertyTypeRepository() {
         if (propertyTypeRepository == null) {
             Repositories repositories = Repositories.getInstance();
             propertyTypeRepository = repositories.getPropertyTypeRepository();
         }
         return propertyTypeRepository;
+    }
+
+    /**
+     * Gets the list of property types.
+     *
+     * @return the property types
+     */
+    public List<PropertyType> getPropertyTypes() {
+        PropertyTypeRepository propertyTypeRepository = getPropertyTypeRepository();
+        return propertyTypeRepository.getPropertyTypeList();
     }
 }
