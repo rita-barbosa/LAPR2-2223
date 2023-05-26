3aa93c53c5d493720282d0ae2aae4b3a467c3583
ana_guterres <1221933@isep.ipp.pt>
2023-05-26 23:35:26 +0100

    Update classes RequestDto, RequestList and RequestMapper [US008][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/RequestMapper.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/RequestMapper.java
@@ -1,4 +1,32 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class RequestMapper {
+
+
+    public static Optional<List<RequestDto>> toDto(List<Request> requests){
+        List<RequestDto> listRequestsDto = new ArrayList<>();
+        for (Request request : requests) {
+            RequestDto dto = toDto(request);
+            listRequestsDto.add(dto);
+        }
+        return Optional.of(listRequestsDto);
+    }
+
+    public static RequestDto toDto(Request request){
+        String propertyAttributes = request.getPropertyAttributes();
+        String businessAttributes = request.getBusinessAttributes();
+        int id = request.getId();
+        String requestDate  = request.getRequestDate().toString();
+
+        return new RequestDto(propertyAttributes, businessAttributes, id, requestDate);
+    }
+
+
+    public static Integer getRequestIdFromDto(Integer requestIdDto){
+        return RequestDto.getRequestId();
+    }
 }
