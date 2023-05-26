e9a35488111cdb2c60ce2a7c267ddbf722930532
ana_guterres <1221933@isep.ipp.pt>
2023-05-26 23:35:55 +0100

    Update classes CommissionTypeDto and CommissionTypeMapper [US008][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/CommissionTypeDto.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/CommissionTypeDto.java
@@ -1,4 +1,14 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 public class CommissionTypeDto {
+
+    private String commissionType;
+
+    public CommissionTypeDto(String commissionType) {
+        this.commissionType = commissionType;
+    }
+
+    public String getCommissionType(){
+        return commissionType;
+    }
 }
--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/CommissionTypeMapper.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/CommissionTypeMapper.java
@@ -1,4 +1,29 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class CommissionTypeMapper {
+
+    public static Optional<List<CommissionTypeDto>> toDto(List<CommissionType> commissionTypes){
+        throw new NotImplementedException();
+//        List<RequestDto> listCommissionTypesDto = new ArrayList<>();
+//        for (CommissionType commissionType : commissionTypes) {
+//            CommissionTypeDto dto = toDto(commissionType);
+//            listCommissionTypesDto.add(dto);
+//        }
+//        return Optional.of(listCommissionTypesDto);
+    }
+
+    public static CommissionTypeDto toDto(CommissionType commissionType){
+        throw new NotImplementedException();
+//        String commissionTypeAttributes = commissionType.getDesignation();
+//
+//        return new CommissionType(commissionTypeAttributes);
+    }
+
 }
