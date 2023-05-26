4dfbc8d95d92968bd3a366a6ba3ae2f9202af092
ana_guterres <1221933@isep.ipp.pt>
2023-05-26 01:57:24 +0100

    Add Dto: CommissionTypeDto and RequestDto [US008][Implementation]

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/RequestDto.java
@@ -0,0 +1,17 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+public class RequestDto {
+
+    private String propertyAttributes;
+
+    private Integer id;
+
+    public String getPropertyAttributes(){
+        return propertyAttributes;
+    }
+
+    public int getRequestId(){
+        return id;
+    }
+
+}
