1452f2bc9c7777259b22717039709367683f8e60
Matilde_Varela <1220683@isep.ipp.pt>
2023-05-26 11:38:32 +0100

    AuthenticationRepository class update [US009] [Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/AuthenticationRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/AuthenticationRepository.java
@@ -5,25 +5,62 @@ import pt.ipp.isep.dei.esoft.project.domain.Agency;
 import pt.isep.lei.esoft.auth.AuthFacade;
 import pt.isep.lei.esoft.auth.UserSession;
 
+/**
+ * The type Authentication repository.
+ */
 public class AuthenticationRepository {
+    /**
+     * The Authentication facade.
+     */
     private final AuthFacade authenticationFacade = new AuthFacade();
 
+    /**
+     * Do login boolean.
+     *
+     * @param email the email
+     * @param pwd   the pwd
+     * @return the boolean
+     */
     public boolean doLogin(String email, String pwd) {
         return authenticationFacade.doLogin(email, pwd).isLoggedIn();
     }
 
+    /**
+     * Do logout.
+     */
     public void doLogout() {
         authenticationFacade.doLogout();
     }
 
+    /**
+     * Gets current user session.
+     *
+     * @return the current user session
+     */
     public UserSession getCurrentUserSession() {
         return authenticationFacade.getCurrentUserSession();
     }
 
+    /**
+     * Add user role boolean.
+     *
+     * @param id          the id
+     * @param description the description
+     * @return the boolean
+     */
     public boolean addUserRole(String id, String description) {
         return authenticationFacade.addUserRole(id, description);
     }
 
+    /**
+     * Add user with role boolean.
+     *
+     * @param name   the name
+     * @param email  the email
+     * @param pwd    the pwd
+     * @param roleId the role id
+     * @return the boolean
+     */
     public boolean addUserWithRole(String name, String email, String pwd, String roleId) {
         if (validatePassword(pwd)) {
             return authenticationFacade.addUserWithRole(name, email, pwd, roleId);
@@ -31,6 +68,12 @@ public class AuthenticationRepository {
         return false;
     }
 
+    /**
+     * Validate password boolean.
+     *
+     * @param pwd the pwd
+     * @return the boolean
+     */
     private boolean validatePassword(String pwd) {
         int passwordLength = 7;
         int digitCount = 0;
@@ -48,5 +91,14 @@ public class AuthenticationRepository {
             }
         }
         return digitCount == 2 && upperCaseLettercount == 3;
+    }
+
+    /**
+     * Get user email string.
+     *
+     * @return the string
+     */
+    public String getUserEmail(){
+       return getCurrentUserSession().getUserId().toString();
     }
 }
