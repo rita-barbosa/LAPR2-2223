651818f397a7a96b34da2f63007dfd27e615c5fe
ana_guterres <1221933@isep.ipp.pt>
2023-05-28 18:10:24 +0100

    Update class RequestMapper + JavaDoc [US008][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/RequestMapper.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/RequestMapper.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The type Request mapper.
+ */
 public class RequestMapper {
 
 
+    /**
+     * To dto optional.
+     *
+     * @param requests the requests
+     * @return the optional
+     */
     public static Optional<List<RequestDto>> toDto(List<Request> requests){
         List<RequestDto> listRequestsDto = new ArrayList<>();
         for (Request request : requests) {
@@ -16,6 +25,12 @@ public class RequestMapper {
         return Optional.of(listRequestsDto);
     }
 
+    /**
+     * To dto request dto.
+     *
+     * @param request the request
+     * @return the request dto
+     */
     public static RequestDto toDto(Request request){
         String propertyAttributes = request.getPropertyAttributes();
         String businessAttributes = request.getBusinessAttributes();
@@ -26,6 +41,12 @@ public class RequestMapper {
     }
 
 
+    /**
+     * Get request id from dto integer.
+     *
+     * @param requestIdDto the request id dto
+     * @return the integer
+     */
     public static Integer getRequestIdFromDto(Integer requestIdDto){
         return RequestDto.getRequestId();
     }
