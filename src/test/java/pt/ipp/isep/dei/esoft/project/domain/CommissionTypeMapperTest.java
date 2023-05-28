3bda97be6bb099faef9bab28479ea83e5bc0e8d8
ana_guterres <1221933@isep.ipp.pt>
2023-05-28 23:22:11 +0100

    Update CommissionTypeMapperTest and CommissionTypeDtoTest [US008][Implementation|Tests]

--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/CommissionTypeMapperTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/CommissionTypeMapperTest.java
@@ -2,15 +2,31 @@ package pt.ipp.isep.dei.esoft.project.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommissionTypeMapperTest {
 
     @Test
-    void toDto() {
-    }
+    void ensureToDtoWorks() {
+        List<CommissionType> commissionTypes = new ArrayList<>();
+        String commType1 = "Something";
+        CommissionType c1 = new CommissionType(commType1);
 
-    @Test
-    void testToDto() {
+        String commType2 = "Nothing";
+        CommissionType c2 = new CommissionType(commType2);
+
+        commissionTypes.add(c1);
+        commissionTypes.add(c2);
+
+        List<CommissionTypeDto> commissionTypeDtos = new ArrayList<>();
+        CommissionTypeDto dto1 = new CommissionTypeDto(c1.getDesignation());
+        CommissionTypeDto dto2 = new CommissionTypeDto(c2.getDesignation());
+        commissionTypeDtos.add(dto1);
+        commissionTypeDtos.add(dto2);
+
+        assertEquals(commissionTypeDtos, CommissionTypeMapper.toDto(commissionTypes));
     }
 }
