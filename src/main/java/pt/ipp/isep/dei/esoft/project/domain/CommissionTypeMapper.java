f4069dc0056b1342a17e1e4ce262359e9f6a044b
ana_guterres <1221933@isep.ipp.pt>
2023-05-28 18:17:33 +0100

    Update class CommissionTypeMapper + JavaDoc [US008][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/CommissionTypeMapper.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/CommissionTypeMapper.java
@@ -1,29 +1,39 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Scanner;
 
+/**
+ * The type CommissionType mapper.
+ */
 public class CommissionTypeMapper {
 
+    /**
+     * To dto optional.
+     *
+     * @param commissionTypes the commissionTypes
+     * @return the optional
+     */
     public static Optional<List<CommissionTypeDto>> toDto(List<CommissionType> commissionTypes){
-        throw new NotImplementedException();
-//        List<RequestDto> listCommissionTypesDto = new ArrayList<>();
-//        for (CommissionType commissionType : commissionTypes) {
-//            CommissionTypeDto dto = toDto(commissionType);
-//            listCommissionTypesDto.add(dto);
-//        }
-//        return Optional.of(listCommissionTypesDto);
+        List<CommissionTypeDto> listCommissionTypesDto = new ArrayList<>();
+        for (CommissionType commissionType : commissionTypes) {
+            CommissionTypeDto dto = toDto(commissionType);
+            listCommissionTypesDto.add(dto);
+        }
+        return Optional.of(listCommissionTypesDto);
     }
 
+    /**
+     * To dto commissionType dto.
+     *
+     * @param commissionType the commission type
+     * @return the commissionType dto
+     */
     public static CommissionTypeDto toDto(CommissionType commissionType){
-        throw new NotImplementedException();
-//        String commissionTypeAttributes = commissionType.getDesignation();
-//
-//        return new CommissionType(commissionTypeAttributes);
+        String commissionTypeAttributes = commissionType.getDesignation();
+
+        return new CommissionTypeDto(commissionTypeAttributes);
     }
 
 }
