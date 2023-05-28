54481ea0bccc6bcc3ad950aa13b901433f632a23
ana_guterres <1221933@isep.ipp.pt>
2023-05-28 18:19:53 +0100

    Update AgentUI [US008][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AgentUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AgentUI.java
@@ -2,6 +2,7 @@ package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
 
 import pt.ipp.isep.dei.esoft.project.ui.console.AcceptOrdersUI;
+import pt.ipp.isep.dei.esoft.project.ui.console.ListRequestsUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.PublishAnnouncementUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
@@ -14,6 +15,7 @@ public class AgentUI implements Runnable {
         List<MenuItem> options = new ArrayList<MenuItem>();
         options.add(new MenuItem("Publish a sale announcement", new PublishAnnouncementUI()));
         options.add(new MenuItem("Accept Orders", new AcceptOrdersUI()));
+        options.add(new MenuItem("List property announcement requests", new ListRequestsUI()));
         int option = 0;
         do {
             option = Utils.showAndSelectIndex(options, "\n\nAgent Menu:");
