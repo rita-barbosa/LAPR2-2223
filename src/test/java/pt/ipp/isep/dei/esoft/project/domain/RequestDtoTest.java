df0b01cc2e403ea617b2cec1e1fc3e9403048c93
ana_guterres <1221933@isep.ipp.pt>
2023-05-30 23:02:43 +0100

    Update RequestDtoTest and RequestMapperTest [US008][Implementation|Tests]

--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/RequestDtoTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/RequestDtoTest.java
@@ -1,35 +1,121 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.esoft.project.application.controller.authorization.AuthenticationController;
+import pt.ipp.isep.dei.esoft.project.repository.AgencyRepository;
+import pt.ipp.isep.dei.esoft.project.repository.AuthenticationRepository;
+import pt.isep.lei.esoft.auth.domain.model.Email;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 class RequestDtoTest {
 
     @Test
-    void getPropertyAttributes() {
+    void ensureGetPropertyAttributesWorks() {
+        String propertyAttributes = "propertyAttributes";
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto("propertyAttributes", "businessAttributes", id, requestDate);
+
+        assertEquals(propertyAttributes, dto.getPropertyAttributes());
     }
 
     @Test
-    void getBusinessAttributes() {
+    void ensureGetBusinessAttributesWorks() {
+        String businessAttributes = "businessAttributes";
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto("propertyAttributes", "businessAttributes", id, requestDate);
+
+        assertEquals(businessAttributes, dto.getBusinessAttributes());
     }
 
     @Test
-    void getRequestDate() {
+    void ensureGetRequestDateWorks() {
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto("propertyAttributes", "businessAttributes", id, requestDate);
+
+        assertEquals(requestDate, dto.getRequestDate());
     }
 
     @Test
-    void getRequestId() {
+    void ensureGetRequestIdWorks() {
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto("propertyAttributes", "businessAttributes", id, requestDate);
+
+        assertEquals(id, dto.getRequestId());
     }
 
     @Test
     void testToString() {
+        String propertyAttributes = "propertyAttributes";
+        String businessAttributes = "businessAttributes";
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto(propertyAttributes, businessAttributes, id, requestDate);
+
+        String expected = "Request ID: 0 \npropertyAttributesbusinessAttributesRequest Date: 2020-10-10\n";
+
+        assertEquals(expected, dto.toString());
     }
 
     @Test
-    void testEquals() {
+    void testEqualsSameObjects() {
+        String propertyAttributes = "propertyAttributes";
+        String businessAttributes = "businessAttributes";
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto(propertyAttributes, businessAttributes, id, requestDate);
+
+        assertEquals(dto, dto);
     }
 
     @Test
-    void testHashCode() {
+    void testEqualsDifferentObjects() {
+        String propertyAttributes = "propertyAttributes";
+        String businessAttributes = "businessAttributes";
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto(propertyAttributes, businessAttributes, id, requestDate);
+        RequestDto dto1 = new RequestDto("something", "sale", 0, "2015-12-13");
+
+        assertNotEquals(dto, dto1);
+    }
+
+    @Test
+    void testHashCodeSameObject() {
+        String propertyAttributes = "propertyAttributes";
+        String businessAttributes = "businessAttributes";
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto(propertyAttributes, businessAttributes, id, requestDate);
+
+        assertEquals(dto.hashCode(), dto.hashCode());
+    }
+
+    @Test
+    void testHashCodeDifferentObject() {
+        String propertyAttributes = "propertyAttributes";
+        String businessAttributes = "businessAttributes";
+        Integer id = 0;
+        String requestDate = "2020-10-10";
+
+        RequestDto dto = new RequestDto(propertyAttributes, businessAttributes, id, requestDate);
+        RequestDto dto1 = new RequestDto("something", "sale", 0, "2015-12-13");
+
+        assertNotEquals(dto.hashCode(), dto1.hashCode());
     }
 }
--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/RequestMapperTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/RequestMapperTest.java
@@ -1,6 +1,14 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.esoft.project.application.controller.authorization.AuthenticationController;
+import pt.ipp.isep.dei.esoft.project.repository.AgencyRepository;
+import pt.ipp.isep.dei.esoft.project.repository.AuthenticationRepository;
+import pt.isep.lei.esoft.auth.domain.model.Email;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,13 +16,61 @@ class RequestMapperTest {
 
     @Test
     void ensureToDtoWorks() {
-    }
+        List<Request> requests = new ArrayList<>();
+        RequestList requestList = new RequestList();
 
-    @Test
-    void testToDto() {
+        Location location = new Location("Saint Avenue", "Heaven", "Sky", "SK", "12345");
+        Agency agency = new Agency(1234, "Make It Home Deluxe", "agency4@this.app", "999 444 5656", location);
+        Employee employee = new Employee(1234, "John Doe", "C12000078",
+                "004-45-6989", "employee@this.app", "agent", "623-456-7890",
+                "New York", "Manhattan", "NY", "10001", "Broadway");
+        agency.addEmployee(employee);
+
+        String ownerEmail = "owner@email.com";
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        uriList.add("https://www.example.com/images/photo123.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3),
+                uriList, "street", "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2500.0), LocalDate.now(), employee);
+
+
+        requests.add(request);
+        requestList.addRequest(request);
+
+        List<RequestDto> requestDtoList = new ArrayList<>();
+        RequestDto dto = new RequestDto(request.getPropertyAttributes(), request.getBusinessAttributes(), request.getId(), request.getRequestDate().toString());
+        requestDtoList.add(dto);
+
+        assertTrue(RequestMapper.toDto(requests).isPresent());
+        assertEquals(requestDtoList, RequestMapper.toDto(requests).get());
     }
 
     @Test
     void getRequestIdFromDto() {
+        Location location = new Location("Saint Avenue", "Heaven", "Sky", "SK", "12345");
+        Agency agency = new Agency(1234, "Make It Home Deluxe", "agency4@this.app", "999 444 5656", location);
+
+        Employee employee = new Employee(1234, "John Doe", "C12000078",
+                "004-45-6989", "employee@this.app", "agent", "623-456-7890",
+                "New York", "Manhattan", "NY", "10001", "Broadway");
+        agency.addEmployee(employee);
+
+        String ownerEmail = "owner@email.com";
+        List<String> uriList = new ArrayList<>();
+        uriList.add("https://www.example.com/images/photo.jpg");
+        uriList.add("https://www.example.com/images/photo123.jpg");
+        Property property = new Property(new PropertyType("land"), (35.5), (89.3),
+                uriList, "street", "city", "district", "state", "12345");
+        Request request = new Request(ownerEmail, property, new Business("sale", 2500.0), LocalDate.now(), employee, 0);
+
+        agency.addRequest(request);
+
+
+        RequestDto dto = new RequestDto(request.getPropertyAttributes(), request.getBusinessAttributes(), request.getId(), request.getRequestDate().toString());
+
+        Integer id = 0;
+
+        assertEquals(id, RequestMapper.getRequestIdFromDto(dto));
     }
 }
