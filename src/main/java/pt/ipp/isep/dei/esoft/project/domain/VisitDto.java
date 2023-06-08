344e98ccb0c0de44319a8f9b58ca9cb2b23b948a
ana_guterres <1221933@isep.ipp.pt>
2023-06-09 00:05:19 +0100

    Add VisitDto & VisitMapper [US015][Implementation]

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/VisitDto.java
@@ -0,0 +1,148 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class VisitDto {
+
+        /**
+         * The Id.
+         */
+        private Integer id = 0;
+        /**
+         * The id iteration variable.
+         */
+        private static int counter = 0;
+        /**
+         * The Start hour.
+         */
+        private Integer startHour;
+        /**
+         * The End hour.
+         */
+        private Integer endHour;
+        /**
+         * The Visit date.
+         */
+        private LocalDate visitDate;
+        /**
+         * The User name.
+         */
+        private String userName;
+        /**
+         * The User phone number.
+         */
+        private String userPhoneNumber;
+        /**
+         * The Acceptance status.
+         */
+        private Boolean acceptanceStatus;
+
+        /**
+         * Instantiates a new Visit.
+         *
+         * @param visitDate       the visit date
+         * @param startHour       the start hour
+         * @param endHour         the end hour
+         * @param userName        the user name
+         * @param userPhoneNumber the user phone number
+         */
+        public VisitDto(LocalDate visitDate, Integer startHour, Integer endHour, String userName, String userPhoneNumber) {
+                this.id = counter++;
+                this.startHour = startHour;
+                this.endHour = endHour;
+                this.visitDate = visitDate;
+                this.userName = userName;
+                this.userPhoneNumber = userPhoneNumber;
+                this.acceptanceStatus = false;
+        }
+
+        public VisitDto(LocalDate visitDate, Integer startHour, Integer endHour, String userName, String userPhoneNumber, Integer id, Boolean acceptanceStatus) {
+                this.id = id;
+                this.startHour = startHour;
+                this.endHour = endHour;
+                this.visitDate = visitDate;
+                this.userName = userName;
+                this.userPhoneNumber = userPhoneNumber;
+                this.acceptanceStatus = acceptanceStatus;
+        }
+
+        /**
+         * Gets visit id.
+         *
+         * @return the id
+         */
+        public Integer getId() {
+                return this.id;
+        }
+
+        /**
+         * Gets start hour.
+         *
+         * @return the start hour
+         */
+        public Integer getStartHour() {
+                return this.startHour;
+        }
+
+        /**
+         * Gets end hour.
+         *
+         * @return the end hour
+         */
+        public Integer getEndHour() {
+                return this.endHour;
+        }
+
+        /**
+         * Gets visit date.
+         *
+         * @return the visit date
+         */
+        public LocalDate getVisitDate() {
+                return this.visitDate;
+        }
+
+        /**
+         * Gets user name.
+         *
+         * @return the user name
+         */
+        public String getUserName() {
+                return userName;
+        }
+
+        /**
+         * Gets user phone number.
+         *
+         * @return the user phone number
+         */
+        public String getUserPhoneNumber() {
+                return userPhoneNumber;
+        }
+
+        /**
+         * Equals boolean.
+         *
+         * @param o the o
+         * @return the boolean
+         */
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                VisitDto visitDto = (VisitDto) o;
+                return Objects.equals(startHour, visitDto.startHour) && Objects.equals(endHour, visitDto.endHour) && Objects.equals(visitDate, visitDto.visitDate) && Objects.equals(userName, visitDto.userName) && Objects.equals(userPhoneNumber, visitDto.userPhoneNumber) && Objects.equals(acceptanceStatus, visitDto.acceptanceStatus);
+        }
+
+        /**
+         * Hash code int.
+         *
+         * @return the int
+         */
+        @Override
+        public int hashCode() {
+                return Objects.hash(startHour, endHour, visitDate, userName, userPhoneNumber, acceptanceStatus);
+        }
+
+}
---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/VisitMapper.java
@@ -0,0 +1,46 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The type Visit mapper.
+ */
+public class VisitMapper {
+
+    /**
+     * Converts a list of Visit objects to a list of VisitDto objects.
+     *
+     * @param visitsList the list of Visit objects
+     * @return the list of VisitDto objects
+     */
+    public List<VisitDto> toDto(List<Visit> visitsList) {
+        List<VisitDto> visitsDtoList = new ArrayList<>();
+
+        for (Visit visit : visitsList) {
+            visitsDtoList.add(toDto(visit));
+        }
+
+        return visitsDtoList;
+    }
+
+    /**
+     * Converts a Visit object to a VisitDto object.
+     *
+     * @param visit the Visit object
+     * @return the VisitDto object
+     */
+    public VisitDto toDto(Visit visit) {
+        Integer id = visit.getId();
+        Integer startHour = visit.getStartHour();
+        Integer endHour = visit.getEndHour();
+        LocalDate visitDate = visit.getVisitDate();
+        String userName = visit.getUserName();
+        String userPhoneNumber = visit.getUserPhoneNumber();
+        Boolean acceptanceStatus = visit.getAcceptanceStatus();
+
+        return new VisitDto(visitDate, startHour, endHour, userName, userPhoneNumber, id, acceptanceStatus);
+
+    }
+}
