2f861b938875d707d528d65318c65dce9027e329
Matilde_Varela <1220683@isep.ipp.pt>
2023-06-13 15:00:14 +0100

    Bootstap update [US017] [Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/Bootstrap.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/Bootstrap.java
@@ -86,8 +86,6 @@ public class Bootstrap implements Runnable {
 
     private void addAgencies() {
         AgencyRepository agencyRepository = Repositories.getInstance().getAgencyRepository();
-        //Integer id, String description, String emailAddress, String phoneNumber, Location location
-
         Location location = new Location("Saint Avenue", "Heaven", "Sky", "SK", "12345");
         Agency agency4 = new Agency(1234, "Make It Home Deluxe", "agency4@this.app", "999 444 5656", location);
         addEmployees(agency4);
@@ -203,7 +201,7 @@ public class Bootstrap implements Runnable {
         String ownerEmail = "owner@email.com";
         String ownerEmail1 = "owner1@email.com";
         String ownerEmail2 = "owner2@email.com";
-        Employee employee = new Employee("employee@this.app", "Agent");
+        Employee employee = agency.getAgentList().get(1);
         CommissionType commissionType = new CommissionType("Commission Type");
         List<String> uriList = new ArrayList<>();
         uriList.add("https://www.example.com/images/photo.jpg");
@@ -249,12 +247,12 @@ public class Bootstrap implements Runnable {
         Announcement a4 = new Announcement(employee, commissionType, 234.0, request3);
         Announcement a5 = new Announcement(employee, commissionType, 234.0, request4);
         Announcement a6 = new Announcement(employee, commissionType, 234.0, request5);
-//        Announcement a7 = new Announcement(employee, 35, request, "23-12-2022", 3245678d);
-//        Announcement a8 = new Announcement(employee, 3495, request1, "23-12-2022", 336678d);
-//        Announcement a9 = new Announcement(employee, 345, request2, "23-12-2022", 32458d);
-//        Announcement a10 = new Announcement(employee, 3435, request3, "23-12-2022", 123245678d);
-//        Announcement a11 = new Announcement(employee, 3, request4, "23-12-2022", 754245678d);
-//        Announcement a12 = new Announcement(employee, 25, request5, "23-12-2022", 45678d);
+        Announcement a7 = new Announcement(employee, 35, request, "23-12-2022", 3245678d);
+        Announcement a8 = new Announcement(employee, 3495, request1, "23-12-2022", 336678d);
+        Announcement a9 = new Announcement(employee, 345, request2, "23-12-2022", 32458d);
+        Announcement a10 = new Announcement(employee, 3435, request3, "23-12-2022", 123245678d);
+        Announcement a11 = new Announcement(employee, 3, request4, "23-12-2022", 754245678d);
+        Announcement a12 = new Announcement(employee, 25, request5, "23-12-2022", 45678d);
 
 
         Order o1 = new Order(2345467.0, new Email("owner@this.app"));
@@ -268,19 +266,19 @@ public class Bootstrap implements Runnable {
         a2.addOrder(o3);
         a3.addOrder(o4);
 
+
         agency.addAnnouncement(a1);
         agency.addAnnouncement(a2);
         agency.addAnnouncement(a3);
         agency.addAnnouncement(a4);
         agency.addAnnouncement(a5);
         agency.addAnnouncement(a6);
-//        agency.addAnnouncement(a7);
-//        agency.addAnnouncement(a8);
-//        agency.addAnnouncement(a9);
-//        agency.addAnnouncement(a10);
-//        agency.addAnnouncement(a11);
-//        agency.addAnnouncement(a12);
+        agency.addAnnouncement(a7);
+        agency.addAnnouncement(a8);
+        agency.addAnnouncement(a9);
+        agency.addAnnouncement(a10);
+        agency.addAnnouncement(a11);
+        agency.addAnnouncement(a12);
 
     }
-
 }
