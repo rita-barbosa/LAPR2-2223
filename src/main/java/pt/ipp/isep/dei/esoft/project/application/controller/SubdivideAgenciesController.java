e37138598130e7b78ba44c992dc56d1ee15c302e
Matilde_Varela <1220683@isep.ipp.pt>
2023-06-13 20:45:07 +0100

    SubdivideAgenciesController class update [US019] [Implementation]

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/SubdivideAgenciesController.java
@@ -0,0 +1,49 @@
+package pt.ipp.isep.dei.esoft.project.application.controller;
+
+import pt.ipp.isep.dei.esoft.project.domain.Partition;
+import pt.ipp.isep.dei.esoft.project.repository.*;
+
+import java.util.List;
+
+public class SubdivideAgenciesController {
+
+    private AgencyRepository agencyRepository;
+
+
+    /**
+     * Instantiates a new Subdivide agencies controller.
+     */
+    public SubdivideAgenciesController() {
+        getAgencyRepository();
+    }
+
+    /**
+     * Instantiates a new Subdivide agencies controller.
+     *
+     * @param agencyRepository         the agency repository
+     */
+    public SubdivideAgenciesController(AgencyRepository agencyRepository) {
+        this.agencyRepository = agencyRepository;
+    }
+
+    private AgencyRepository getAgencyRepository() {
+        if (agencyRepository == null) {
+            Repositories repositories = Repositories.getInstance();
+            agencyRepository = repositories.getAgencyRepository();
+        }
+        return agencyRepository;
+    }
+
+    public List<String> getAgenciesPartitions(){
+        return Partition.getSubLists(agencyRepository.getDealsNumberOfAgencies());
+    }
+
+
+
+
+
+
+
+
+
+}
