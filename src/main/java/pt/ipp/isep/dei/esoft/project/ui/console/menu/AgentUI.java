b2fed702c7358dd04519e799d2a2dba21894140c
ana_guterres <1221933@isep.ipp.pt>
2023-06-14 22:58:48 +0100

    Update AgentUI to allow using JavaFx [US015][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AgentUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/AgentUI.java
@@ -1,6 +1,8 @@
 package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
 
+import pt.ipp.isep.dei.esoft.project.ui.console.menu.gui.ListVisitsApp;
+import javafx.application.Application;
 import pt.ipp.isep.dei.esoft.project.ui.console.AcceptOrdersUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.ListRequestsUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.ListVisitsUI;
@@ -18,13 +20,26 @@ public class AgentUI implements Runnable {
         options.add(new MenuItem("Accept Orders", new AcceptOrdersUI()));
         options.add(new MenuItem("List property announcement requests", new ListRequestsUI()));
         options.add(new MenuItem("List booking requests", new ListVisitsUI()));
+        options.add(new MenuItem("List booking requests (Java Fx)", new ListVisitsApp()));
         int option = 0;
         do {
             option = Utils.showAndSelectIndex(options, "\n\nAgent Menu:");
 
             if ((option >= 0) && (option < options.size())) {
-                options.get(option).run();
+                if (options.get(option).getGui() == null) {
+                    options.get(option).run();
+                } else if (options.get(option).getUi() == null) {
+                    try {
+                        Application.launch(options.get(option).getGui().getClass());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
+
+//            if ((option >= 0) && (option < options.size())) {
+//                options.get(option).run();
+//            }
         } while (option != -1);
     }
 }
