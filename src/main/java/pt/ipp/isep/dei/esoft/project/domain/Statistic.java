6faed753a32a080b71fe836f2f3051ef9047927f
rita_barbosa <1220841@isep.ipp.pt>
2023-06-15 19:02:18 +0100

    Update java reflection [US018][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Statistic.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Statistic.java
@@ -17,28 +17,28 @@ public class Statistic {
     public Statistic(List<List<Double>> dealsDataList, RegressionModelType regressionModelType) throws ReflectiveOperationException {
         this.dealsDataList = dealsDataList;
         this.regressionModelType = regressionModelType;
-//        String className = "pt.ipp.isep.dei.esoft.project.domain." + regressionModelType.getDesignation().replace(" ", "");
-//        try {
-//            Class<?> oClass = Class.forName(className);
-//            if (RegressionModel.class.isAssignableFrom(oClass)) {
-//                Constructor<?> constructor = oClass.getDeclaredConstructor(List.class);
-//                RegressionModel r = (RegressionModel) constructor.newInstance(dealsDataList);
-//                this.report = r.getRegressionReport();
-//                this.forecastValues = r.getForecastValues();
-//            }
-//        } catch (ReflectiveOperationException e) {
-//            throw new ReflectiveOperationException("ERROR: Invalid Regression Model Type (" + className + " is not available.)");
-//        }
-        if (regressionModelType.isSimpleLinear()) {
-            SimpleLinear s = new SimpleLinear(dealsDataList);
-            this.report = s.getRegressionReport();
-            this.forecastValues = s.getForecastValues();
-        } else if (regressionModelType.isMultilinear()) {
-            Multilinear m = new Multilinear(dealsDataList);
-            this.report = m.getRegressionReport();
-            this.forecastValues = m.getForecastValues();
-
+        String className = "pt.ipp.isep.dei.esoft.project.domain." + regressionModelType.getDesignation().replace(" ", "");
+        try {
+            Class<?> oClass = Class.forName(className);
+            if (RegressionModel.class.isAssignableFrom(oClass)) {
+                Constructor<?> constructor = oClass.getDeclaredConstructor(List.class);
+                RegressionModel r = (RegressionModel) constructor.newInstance(dealsDataList);
+                this.report = r.getRegressionReport();
+                this.forecastValues = r.getForecastValues();
+            }
+        } catch (ReflectiveOperationException e) {
+            throw new ReflectiveOperationException("ERROR: Invalid Regression Model Type (" + className + " is not available.)");
         }
+//        if (regressionModelType.isSimpleLinear()) {
+//            SimpleLinear s = new SimpleLinear(dealsDataList);
+//            this.report = s.getRegressionReport();
+//            this.forecastValues = s.getForecastValues();
+//        } else if (regressionModelType.isMultilinear()) {
+//            Multilinear m = new Multilinear(dealsDataList);
+//            this.report = m.getRegressionReport();
+//            this.forecastValues = m.getForecastValues();
+//
+//        }
     }
 
     public List<List<Double>> getDealsDataList() {
