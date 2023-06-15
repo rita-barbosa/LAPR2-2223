5f4e61f498997b3119b3f99a2eade97a04cbe8fb
rita_barbosa <1220841@isep.ipp.pt>
2023-06-15 16:29:21 +0100

    Update Partition [US019][Implementation]

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Partition.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Partition.java
@@ -25,83 +25,85 @@ public class Partition {
         List<String> subListsString = new ArrayList<>();
         subListsString.add(this.getPartitionInfo(subList1, 1));
         subListsString.add(this.getPartitionInfo(subList2, 2));
-        subListsString.add("Difference : " + this.minDifference);
+        subListsString.add(String.valueOf(this.minDifference));
         return subListsString;
     }
 
     private void getPartitions(List<Integer> listOfDeals) {
-        int listOfDealsSize = getListSize(listOfDeals);
-        int size = getPowerOfTwo(listOfDealsSize);
+        int listOfDealsSize = getListSize(listOfDeals); // 3n+3 : O(n)
+        int size = getPowerOfTwo(listOfDealsSize); //3n + 2 :
 
-        this.minDifference = Integer.MAX_VALUE;
+        this.minDifference = Integer.MAX_VALUE; // 1A
 
-        for (int i = 0; i < size; i++) {
-            List<Integer> subList1 = new ArrayList<>(listOfDeals);
-            List<Integer> subList2 = new ArrayList<>(listOfDeals);
-            int subListSum1 = 0;
-            int subListSum2 = 0;
+        for (int i = 0; i < size; i++) { // (2^n -1) + 1 = 2^n
+            List<Integer> subList1 = new ArrayList<>(listOfDeals); // (2^n -1) A
+            List<Integer> subList2 = new ArrayList<>(listOfDeals); // (2^n -1)  A
+            int subListSum1 = 0; // (2^n -1) A
+            int subListSum2 = 0; // (2^n -1) A
 
-            String binary = decimalToBinary(i);
+            String binary = decimalToBinary(i); // 7n + 4 * (2^n -1)
 
-            for (int j = 0; j < binary.length(); j++) {
-                if (binary.charAt(j) == '0') {
-                    subList1.set(j, 0);
-                    subListSum2 += listOfDeals.get(j);
+            for (int j = 0; j < binary.length(); j++) { // (n + 1) * (2^n -1)
+                if (binary.charAt(j) == '0') { // n * (2^n -1) C
+                    subList1.set(j, 0); // n * (2^n -1) A
+                    subListSum2 += listOfDeals.get(j); // n * (2^n -1) A + (n) * (2^n -1)op
                 } else {
                     subList2.set(j, 0);
                     subListSum1 += listOfDeals.get(j);
                 }
             }
-            if (binary.length() < size) {
-                for (int k = binary.length(); k < listOfDealsSize; k++) {
-                    subList1.set(k, 0);
-                    subListSum2 += listOfDeals.get(k);
+
+            if (binary.length() < size) { // (2^n -1) C
+                for (int k = binary.length(); k < listOfDealsSize; k++) { // (n - 1) + 1 = n * (2^n -1)
+                    subList1.set(k, 0); // (n-1) * (2^n -1) A
+                    subListSum2 += listOfDeals.get(k); // (n-1) * (2^n -1) A +  (n-1) * (2^n -1)op
                 }
             }
-            int difference = calculateDifference(subListSum1, subListSum2);
-            if (this.minDifference > difference) {
-                this.minDifference = difference;
-                this.subList1 = subList1;
-                this.subList2 = subList2;
+
+            int difference = calculateDifference(subListSum1, subListSum2); // 6 * 2^n-1 A
+            if (this.minDifference > difference) { // 2^n-1 C
+                this.minDifference = difference; //2^n-1 A
+                this.subList1 = subList1; // 2^n-1 A
+                this.subList2 = subList2; //2^n-1 A
             }
         }
     }
 
-    private static int getListSize(List<Integer> list) {
-        int counter = 0;
+    private static int getListSize(List<Integer> list) { // 1 + (n+1) + (n+1) + n + 1 = 3n +3
+        int counter = 0; // 1A
 
-        for (Integer num : list) {
-            counter++;
+        for (Integer num : list) { // (n + 1) AouI + (n + 1) C
+            counter++; //(n)AouI
         }
-        return counter;
+        return counter;//1R
     }
 
-    public static int getPowerOfTwo(int listSize) {
-        int n = 2;
-        if (listSize == 0) {
+    public static int getPowerOfTwo(int listSize) { // 1 + 1 + 1 + n + (n-1) + (n-1) + 1 = 3n + 2
+        int n = 2; //1A
+        if (listSize == 0) {  //1C
             return 0;
-        } else if (listSize == 1) {
+        } else if (listSize == 1) { //1C
             return 1;
         } else {
-            for (int i = 0; i < listSize - 1; i++) {
-                n = n * 2;
+            for (int i = 0; i < listSize - 1; i++) { // (n-1) + 1 = n
+                n = n * 2; // (n-1)op + (n-1)1A
             }
-            return n - 1;
+            return n - 1; //1R
         }
     }
 
-    public static String decimalToBinary(int decimal) {
-        if (decimal == 0) {
+    public static String decimalToBinary(int decimal) { // 1 + 1 + 1 + (n +1) + (n) + (n) + (n) +  (n) + (n) + (n) = 7n + 4
+        if (decimal == 0) { //1C
             return "0";
         }
-        String binary = "";
-        while (decimal > 0) {
-            int bit = decimal % 2; //1Op + 1A
-            binary = bit + binary;
-            decimal /= 2;
+        String binary = ""; // 1A
+        while (decimal > 0) { // (n) + 1C
+            int bit = decimal % 2; // (n)op + (n)A
+            binary = bit + binary; // (n)op + (n)A
+            decimal = decimal / 2; // (n)op + (n)A
         }
 
-        return binary;
+        return binary; //1R
     }
 
     private String getPartitionInfo(List<Integer> subList, int idx) {
@@ -115,11 +117,11 @@ public class Partition {
         return s.toString();
     }
 
-    private static int calculateDifference(int sum1, int sum2) {
-        int dif = (sum2) - (sum1);
-        if (dif < 0) {
-            dif = (-dif);
+    private static int calculateDifference(int sum1, int sum2) { // 1+1+1+1+1+1 = 6
+        int dif = (sum2) - (sum1); //1op + 1A
+        if (dif < 0) { // 1C
+            dif = (-dif); //1op +1A
         }
-        return dif;
+        return dif; //1R
     }
 }
